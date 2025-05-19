48e5a1b8ed566483f89c5f3817a5d75a67cb78b0
Kirill Pet <job.mori.1967@gmail.com>
2025-05-19 19:59:59 +0300

    slider адаптивность

--- layout/src/main.js
+++ layout/src/main.js
@@ -7,4 +7,14 @@ const swiper = new Swiper('.swiper-slider', {
   direction: 'horizontal',
   slidesPerView: "auto",
   spaceBetween: 70,
+  breakpoints: {
+    0:{
+      slidesPerView: 1.2,
+      spaceBetween: 30,
+    },
+    1024:{
+      slidesPerView: "auto",
+      spaceBetween: 70,
+    },
+  }
 });
