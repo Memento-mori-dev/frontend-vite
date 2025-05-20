3dc15a9ecfaa396d692e954ecd62ba860c88bee8
Kirill Pet <job.mori.1967@gmail.com>
2025-05-20 23:26:40 +0300

    сделал бесконечный slider

--- layout/src/main.js
+++ layout/src/main.js
@@ -10,21 +10,3 @@ new OpenModal(controllerScroll.stop.bind(controllerScroll));
 new CloseModal(controllerScroll.play.bind(controllerScroll));
 
 
-const swiper = new Swiper('.swiper-slider', {
-  loop: true,
-  speed: 2000,
-  autoplay: {
-    delay: 0,
-    // disableOnInteraction: false 
-  },
-  breakpoints: {
-    0:{
-      slidesPerView: 1.2,
-      spaceBetween: 30,
-    },
-    1024:{
-      slidesPerView: 1.7,
-      spaceBetween: 70,
-    },
-  }
-});
