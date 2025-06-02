d9d804fdffcd88feaa4c29305759ebbe362e4952
Kirill Pet <job.mori.1967@gmail.com>
2025-06-02 16:54:15 +0300

    watch для pc

---
+++ layout/src/script/swiper.js
@@ -0,0 +1,21 @@
+// watch
+
+const arrSwiperWatch = document.querySelectorAll('.watch__item'),
+        objSwiperWatch = {};
+
+
+
+arrSwiperWatch.forEach((swiper, index) => {
+    const swiperClass = `.watch__item-${index}`;
+
+    objSwiperWatch[`swiper-${index}`] = new Swiper(swiperClass, {
+        slidesPerView: 2.3,
+        spaceBetween: 70,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.button-slider__item--next',
+            prevEl: '.button-slider__item--prev',
+        },
+    });
+    
+})
