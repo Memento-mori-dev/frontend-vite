1810af0dc4cf429985bcacc841951269439fcf77
Kirill Pet <job.mori.1967@gmail.com>
2025-08-07 01:54:53 +0300

    js swiperProjects

--- layout/src/main.js
+++ layout/src/main.js
@@ -1,3 +1,19 @@
 import { useDynamicAdapt } from './script/dynamicAdapt';
 
 useDynamicAdapt();
+
+// input mask
+Inputmask({"mask": "+7 (999) 999-99-99"}).mask(document.querySelectorAll('[data-js-phone]'));
+
+// swiper
+
+const swiperProjects = new Swiper('.projects-swiper', {
+    slidesPerView: 2.3,
+    spaceBetween: 20,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.projects .button-slider__item--next',
+        prevEl: '.projects .button-slider__item--prev',
+    },
+})
+
