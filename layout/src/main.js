40c68d526d49250f63d2de0fd244cf87bf5c16ea
Kirill Pet <job.mori.1967@gmail.com>
2025-08-07 02:09:02 +0300

    js swiperCompanies

--- layout/src/main.js
+++ layout/src/main.js
@@ -7,13 +7,38 @@ Inputmask({"mask": "+7 (999) 999-99-99"}).mask(document.querySelectorAll('[data-
 
 // swiper
 
-const swiperProjects = new Swiper('.projects-swiper', {
-    slidesPerView: 2.3,
-    spaceBetween: 20,
-    // Navigation arrows
-    navigation: {
-        nextEl: '.projects .button-slider__item--next',
-        prevEl: '.projects .button-slider__item--prev',
-    },
-})
+if(document.querySelector('.projects')){
+    const swiperProjects = new Swiper('.projects-swiper', {
+        slidesPerView: 2.3,
+        spaceBetween: 20,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.projects .button-slider__item--next',
+            prevEl: '.projects .button-slider__item--prev',
+        },
+    })
+}
 
+if (document.querySelector('.trust__companies')) {
+    const swiperCompanies = new Swiper('.companies-swiper', {
+        slidesPerView: 4.76,
+        spaceBetween: 12,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        loop: true
+    })
+}
+
+if (document.querySelector('.trust__documents')) {
+    const swiperDocuments = new Swiper('.documents-swiper', {
+        slidesPerView: 3.67,
+        spaceBetween: 12,
+
+        navigation: {
+            nextEl: '.trust__documents .button-slider__item--next',
+            prevEl: '.trust__documents .button-slider__item--prev',
+        },
+    })
+}
