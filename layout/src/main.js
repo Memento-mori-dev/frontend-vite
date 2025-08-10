42cb03bf05d29485df211f6d57e456f6fedeedd1
Kirill Pet <job.mori.1967@gmail.com>
2025-08-10 20:29:51 +0300

    reviews

--- layout/src/main.js
+++ layout/src/main.js
@@ -43,7 +43,9 @@ if (document.querySelector('.trust__documents')) {
             prevEl: '.trust__documents .button-slider__item--prev',
         },
     })
+}
 
+if (document.querySelector('.trust__documents') || document.querySelector('.reviews')) {
     Fancybox.bind('[data-fancybox]', {
         //
     });
